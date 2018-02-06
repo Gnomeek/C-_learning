094a223e2205914075d0cea5c109e8a9cb5e710e
Gnomeek <gnomeek@Outlook.com>
2018-02-06 23:01:13 +0800

    20180206

--- C++/main.cpp
+++ C++/main.cpp
@@ -42,9 +42,16 @@ int main()
         std::cout<<curr <<" occurs " << count <<std::endl;
     }
     */
-    int i=100, Sum=0;
+    /*int i=100, Sum=0;
     for(int i =0;i!=10;++i)
         Sum+=i;
-    std::cout<<i<<" "<<Sum<<std::endl;
+    std::cout<<i<<" "<<Sum<<std::endl;*/
+    int i,&r1=i;
+    i=5;
+    r1=10;
+    //double d=5.2,&r2=d;
+    //r1=r2;
+    std::cout<<r1<<" "<<i<<std::endl;
+
     return 0;
 }
